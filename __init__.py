514b3302a6e3edee9d61927c14490d8a2ec19f48
Error44 <hello@error44.tech>
2025-05-06 13:38:46 +0200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .client import WavecordClient
+from .player import Player
+from .track import Track
+from .node import Node
+from .queue import Queue
+from .exceptions import *
+from .events import *
