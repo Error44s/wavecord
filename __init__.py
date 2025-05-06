b12a2524773a344506a01e7d209bdaca2833b1a8
Error44 <hello@error44.tech>
2025-05-06 21:00:48 +0200

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from .client import WavecordClient
-from .player import Player
-from .track import Track
-from .node import Node
-from .queue import Queue
-from .exceptions import *
-from .events import *
