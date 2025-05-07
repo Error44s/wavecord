a23d0d2adca3b127994b471da1b85ef8b54096d6
Error44 <hello@error44.tech>
2025-05-07 17:33:13 +0200

    bugfix: ci update exceptions.py

--- wavecord/exceptions.py
+++ wavecord/exceptions.py
@@ -1,28 +1,28 @@
 class WavecordException(Exception):
     """Base exception for all Wavecord-related errors."""
-    pass
+    ...
 
 
 class NodeConnectionError(WavecordException):
     """Raised when a connection to the Lavalink node fails."""
-    pass
+    ...
 
 
 class NodeNotConnected(WavecordException):
     """Raised when attempting to use a node that isn't connected."""
-    pass
+    ...
 
 
 class PlayerNotConnected(WavecordException):
     """Raised when trying to control a player that hasn't connected to a voice channel."""
-    pass
+    ...
 
 
 class TrackLoadError(WavecordException):
     """Raised when a track fails to load or is invalid."""
-    pass
+    ...
 
 
 class InvalidTrackType(WavecordException):
     """Raised when a given object is not a valid Track instance."""
-    pass
+    ...
