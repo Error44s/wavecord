750066c66529a1bc1b674a2d007d4a2cfe876ba7
Error44 <hello@error44.tech>
2025-05-07 17:39:23 +0200

    bugfix & code: update queue.py

--- wavecord/queue.py
+++ wavecord/queue.py
@@ -1,40 +1,54 @@
-from collections import deque
-from typing import Deque, Optional, List
-from .track import Track
+from __future__ import annotations
+
 import random
+from collections import deque
+from typing import Deque, Iterator, List, Optional
+
+from .track import Track
 
 
 class TrackQueue:
-    """A simple FIFO queue to store tracks."""
+    """A simple FIFO queue that manages upcoming tracks."""
 
     def __init__(self) -> None:
         self._queue: Deque[Track] = deque()
 
-    def put(self, track: Track) -> None:
+    def add(self, track: Track) -> None:
+        """Add a single track to the queue."""
         self._queue.append(track)
 
-    def put_many(self, tracks: List[Track]) -> None:
+    def add_many(self, tracks: List[Track]) -> None:
+        """Add multiple tracks to the queue."""
         self._queue.extend(tracks)
 
-    def get(self) -> Optional[Track]:
+    def pop(self) -> Optional[Track]:
+        """Remove and return the next track."""
         return self._queue.popleft() if self._queue else None
 
     def peek(self) -> Optional[Track]:
+        """Return the next track without removing it."""
         return self._queue[0] if self._queue else None
 
-    def clear(self) -> None:
-        self._queue.clear()
-
     def shuffle(self) -> None:
+        """Shuffle the queue."""
         temp = list(self._queue)
         random.shuffle(temp)
         self._queue = deque(temp)
 
+    def clear(self) -> None:
+        """Clear the queue."""
+        self._queue.clear()
+
+    def is_empty(self) -> bool:
+        """Check if the queue is empty."""
+        return not self._queue
+
+    def to_list(self) -> List[Track]:
+        """Return the queue as a list."""
+        return list(self._queue)
+
     def __len__(self) -> int:
         return len(self._queue)
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[Track]:
         return iter(self._queue)
-
-    def to_list(self) -> List[Track]:
-        return list(self._queue)
